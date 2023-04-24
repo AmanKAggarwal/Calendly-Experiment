db48c8acbddf56a2c826a29da7a552a977605707
Aman Aggarwal <akaggarw@ucsd.edu>
2023-04-24 14:24:29 -0700

    Initial Commit

---
+++ CalendlyExperiment/CalendlyExperimentApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalendlyExperimentApp.swift
+//  CalendlyExperiment
+//
+//  Created by Aman Aggarwal on 4/24/23.
+//
+
+import SwiftUI
+
+@main
+struct CalendlyExperimentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
